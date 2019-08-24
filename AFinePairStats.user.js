8f0f8e5fb826d8f73037d165d7751e535f65880f
Cryo99 <Cryo99@users.noreply.github.com>
2019-08-24 14:37:00 +0100

    Update AFinePairStats.user.js

--- AFinePairStats.user.js
+++ AFinePairStats.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name        AFinePairStats
+// @name        A Fine Pair Stats
 // @namespace   http://www.cryotest.com/
 // @description Adds your A Fine Pair stats badge onto your profile page and A Fine Pair cache pages on geocaching.com.
 // @license     GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
